0cea7d8ab5471c4669865c15fbe744bc57833c1f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2023-12-07 21:23:25 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
 function App() {
-  return <div>Hello React</div>;
+  return <div className="steps">
+    <div className="numbers">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
 
- 
+    <p className="message">Hello</p>
+
+    <div className="buttons">
+      <button>Previous</button>
+      <button>Next</button>
+    </div>
+  </div> 
 }
-
 export default App;
