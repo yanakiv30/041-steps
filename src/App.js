b1dbf72e284872f2a1e468a8eb376e51898bdfdc
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2023-12-08 06:56:21 +0200

    className-active

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+ 
+
 function App() {
+  const step = 2;
   return <div className="steps">
     <div className="numbers">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div className={`${step>=1 ? "active" : ""}`}>1</div>
+      <div className={`${step>=2 ? "active" : ""}`}>2</div>
+      <div className={`${step>=3 ? "active" : ""}`}>3</div>
     </div>
 
-    <p className="message">Hello</p>
+    <p className="message">Step {step}: {messages[step-1]} </p>
 
     <div className="buttons">
-      <button>Previous</button>
-      <button>Next</button>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+        Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+        Next</button>
     </div>
   </div> 
 }
