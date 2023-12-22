41bb994e6fe36b5937195960596ae63d80b13834
yanakiv30 <yanakiv30@gmail.com>
2023-12-22 13:33:50 +0200

    reusable-buttons

--- src/App.js
+++ src/App.js
@@ -43,26 +43,29 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2",
-              color: "#fff" }}
+            <Button bgColor="#7950f2"
+              textColor="#fff"
               onClick={handlePrevious}
-            >
-              Previous{" "}
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", 
-              color: "#fff" }}
+              text="Previous" />
+            <Button bgColor="#7950f2"
+              textColor="#fff"
               onClick={handleNext}
-            >
-              Next
-            </button>
+              text="Next" />
+            
           </div>
         </div>
       )}
     </div>
-
   );
+}
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return <button style={{ backgroundColor: bgColor, color: textColor }}
+    onClick={onClick}
+  >
+    {text}
+  </button>
+
 }
 
 
